b35c857f2d4e947106a4942cb5c5b14de95462ae
Vincent Tocco <vtocco@Vincents-MacBook-Pro.local>
2019-09-06 09:46:29 -0700

    initial commit of MI

---
+++ intro/main.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+
+# https://github.com/mtobeiyf/keras-flask-deploy-webapp
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+from keras.applications.resnet50 import ResNet50
+
+model = ResNet50(weights='imagenet')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['image']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        pred_class = decode_predictions(preds, top=1)  # ImageNet Decode
+        result = str(pred_class[0][0][1])  # Convert to string
+        return result
+
+    return None
+
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
